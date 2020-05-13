073d00a6ef9c92ee011777c5a03aae3e42430a3d
Zach Nicholson <raiste.doctow@gmail.com>
2020-05-12 23:11:22 -0500

    logic for trying to take more than one item

--- scripts/items_dom.js
+++ scripts/items_dom.js
@@ -1,3 +1,15 @@
+const availItemsContainer = document.querySelector('.items_available_container')
+const addAvailableItemstoDOM = (object) => {
+    availItemsContainer.innerHTML += `
+    <section class='item_available' id='available-${object.id}'>
+        <p>${object.name}</p>
+        <div id='tooltip-${object.id}'></div>
+        <button class='item_select' id='select-${object.id}'>Pick Up</button>
+    </section>
+    `
+}
+
+const invContainer = document.querySelector('.inventory_container')
 const addInventoryToDOM = (object) => {
     let newItem = document.createElement('section')
     newItem.id = `item-${object.id}`
@@ -7,22 +19,12 @@ const addInventoryToDOM = (object) => {
     <p>Description: ${object.description}</p>
     <button id="delete-${object.id}">Drop</button>
     `
-    document.querySelector('.inventory_container').appendChild(newItem);
+    invContainer.appendChild(newItem);
 }
 
 const removeInventoryFromDOM = (id) => {
     let currentItem = document.querySelector(`#item-${id}`)
     currentItem.parentElement.removeChild(currentItem)
-}
-
-const addAvailableItemstoDOM = (object) => {
-    document.querySelector('.items_available_container').innerHTML += `
-    <section class='item_available' id='available-${object.id}'>
-        <p>${object.name}</p>
-        <div id='tooltip-${object.id}'></div>
-        <button class='item_select' id='select-${object.id}'>Pick Up</button>
-    </section>
-    `
 }
 
 export default { addInventoryToDOM, removeInventoryFromDOM, addAvailableItemstoDOM }
